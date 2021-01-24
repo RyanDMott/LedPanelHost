72868d79ffd908885f75068cfaaee9b318b2b09e
Ryan Mott <ryan@towerccc.com>
2021-01-24 16:10:46 -0500

    Switch to FastLED, but only show first image transmitted

--- arduino/LedTarget/LedTarget.ino
+++ arduino/LedTarget/LedTarget.ino
@@ -1,64 +1,53 @@
 #include "SerialReceiver.hpp"
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
 
 using namespace towerccc::LED;
 
 #define LED_COUNT (8*32)
 #define BYTES_PER_LED 3
 #define BUFFER_SIZE (LED_COUNT * BYTES_PER_LED + 1)
-// #define BUFFER_SIZE (10 + 1)
 
-#ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-#endif
+#define CHIPSET     WS2811
+#define COLOR_ORDER GRB
+CRGB leds[LED_COUNT];
 
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1:
 #define LED_PIN     3
 
 // NeoPixel brightness, 0 (min) to 255 (max)
 #define BRIGHTNESS 16
 
-// Declare our NeoPixel strip object:
-// Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-// Argument 1 = Number of pixels in NeoPixel strip
-// Argument 2 = Arduino pin number (most are valid)
-// Argument 3 = Pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-
-
 void setup() {
+  delay( 3000 ); // power-up safety delay
+  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, LED_COUNT).setCorrection( TypicalSMD5050 );
+  FastLED.setBrightness( BRIGHTNESS );
+
   // put your setup code here, to run once:
-  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip.show();            // Turn OFF all pixels ASAP
-  strip.setBrightness(BRIGHTNESS);
+  
+  for(int i = 0; i < LED_COUNT; i++)
+    leds[i] = CRGB(0,0,0);
+
+  FastLED.show();
+    
   SerialReceiver receiver = SerialReceiver();
   byte buffer[BUFFER_SIZE];
   while(true)
   {
-    // receiver.FetchInto(buffer, LED_COUNT + 1);
-    if(!receiver.FetchInto(buffer, BUFFER_SIZE))
+    if (!receiver.FetchInto(buffer, BUFFER_SIZE))
       continue;
 
     int i;
     for(i = 0; i < LED_COUNT; i++)
     {
-      strip.setPixelColor(i, strip.gamma32(strip.Color(
-        buffer[3*i+0], 
-        buffer[3*i+1], 
-        buffer[3*i+2]))); // add fourth element (zero)*/
+      leds[i].red = buffer[3*i+0];
+      leds[i].green = buffer[3*i+1];
+      leds[i].blue = buffer[3*i+2];     
     }
   
-    strip.show();
+    FastLED.show();
+    break;
   }  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
+  FastLED.delay(8000);
 }
